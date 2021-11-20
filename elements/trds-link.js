c7a9dd458fea708e978b932caefb00182a54448b
danielkormos1993 <danielkormos1993@gmail.com>
2021-11-20 12:07:15 +0100

    stuck at link and link text, lookin into new tech

---
+++ elements/trds-link.js
@@ -0,0 +1,24 @@
+﻿customElements.define('trds-link', class trdsLink extends HTMLElement{
+
+    constructor(){
+        super();
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                :host{
+                    max-width: var(--trds-element--max-width);
+                }
+                :host > span{
+                    -webkit-text-size-adjust: 100%;
+                    color: inherit;
+                    font-size: inherit;
+                    line-height: inherit;
+                }
+            </style>
+            <a href="${this.getAttribute('href')}">
+                <slot></slot>
+            </a>    
+        `
+    }
+
+});
