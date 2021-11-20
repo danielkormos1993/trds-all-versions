8f2855f1700a7f59a94fc4c59f053f0332baecdb
danielkormos1993 <danielkormos1993@gmail.com>
2021-11-20 09:10:49 +0100

    added documentation to trds-stack

--- layout/trds-stack.js
+++ layout/trds-stack.js
@@ -1,4 +1,9 @@
-﻿import '../base/layout.js';
+﻿// USAGE: trds-stack
+// its a basic element which will put space beetween elements inside
+// modify the spacing with the gap style property(eg. <trds-stack style="gap: var(--trds-space--s,m,l etc.")
+// TODO: consider the align content property later
+
+import '../base/layout.js';
 
 customElements.define('trds-stack', class trdsStack extends HTMLElement{
 
