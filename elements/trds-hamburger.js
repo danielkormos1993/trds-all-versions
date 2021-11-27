5ace2282e5f3d04e067e9565e2352cfeb7a3068a
Daniel Kormos <danielkormos1993@gmail.com>
2021-11-27 21:11:05 +0100

    added hamburger element

---
+++ elements/trds-hamburger.js
@@ -0,0 +1,53 @@
+// usage: trds-hamburger
+// change color with color style attribute
+// setActive and removeActive for toggling states
+
+customElements.define('trds-hamburger', class TrdsHamburger extends HTMLElement{
+
+    constructor(){
+        super();
+
+        this.attachShadow({mode: 'open'}).innerHTML = `
+            <style>
+                :host{
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    width: 28px;
+                    height: 17px;
+                    cursor: pointer;
+                }
+                div{
+                    display: block;
+                    height: 3px;
+                    background-color: currentColor;
+                    transition: transform .25s, opacity .25s;
+                }
+            
+                :host([active]) div:nth-of-type(1){
+                    transform: rotate(45deg) translate(6px,4px);
+                }
+                
+                :host([active]) div:nth-of-type(2){
+                    opacity: 0;
+                }
+                
+                :host([active]) div:nth-of-type(3){
+                    transform: rotate(-45deg) translate(6px,-4px);
+                }
+            </style>
+            <div></div>
+            <div></div>
+            <div></div>
+        `
+    }
+
+    setActive = () => {
+        this.setAttribute('active', '');
+    }
+
+    removeActive = () => {
+        this.removeAttribute('active');
+    }
+
+});
