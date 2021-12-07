2b6716e7348a2cbe1d588edc58d84bf8b0d0a908
Daniel Kormos <danielkormos1993@gmail.com>
2021-12-07 05:54:30 +0100

    timeline finished

--- components/trds-timeline.js
+++ components/trds-timeline.js
@@ -2,97 +2,102 @@
 // add trds-timeline__step element for a step which MUST HAVE A NUMBER attribute which displayed as count in timeline
 // it automatically set timeline to row oriented if all elements can fit 
 
-import '../base/colors.js';
+import '../base/theme.js';
 import '../base/layout.js';
-import '../elements/trbs-icon.js';
+import '../elements/trds-icon.js';
 
-let TrbsTimelineStyle = document.createElement('style');
-TrbsTimelineStyle.id = 'trbs-timeline-style';
-TrbsTimelineStyle.textContent = `
-    trbs-timeline{
-        display: grid;
-        border-left: 3px solid var(--trbs-color--red);
-        gap: var(--trbs-space--xl);
-        padding-left: 2rem;
-        margin-left: 1rem;
-    }
-    .trbs-timeline--row{
-        grid-template-columns: repeat(auto-fit, minmax(1px, 1fr));
-        border-left: none;
-        border-top: 3px solid var(--trbs-color--red);
-        padding-left: 0;
-        margin-left: 0;
-        padding-top: 3rem;
-    }
-    trbs-timeline__step{
-        display: block;
-        position: relative;
-    }
-    trbs-timeline__step:last-child{
-        padding-bottom: 2.85rem;
-    }
-    .trbs-timeline--row trbs-timeline__step:last-child{
-        padding-bottom: 0;
-    }
-    trbs-timeline__step:before,
-    trbs-timeline__finish-flag{
-        content: attr(number);
-        width: 2rem;
-        height: 2rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: var(--trbs-color--red);
-        border-radius: 50%;
-        position: absolute;
-        left: -3.05rem;
-        top: -.28rem;
-        font-size: var(--trbs-size--m);
-        font-weight: bold;
-    }
-    trbs-timeline__step[digit-number="3"]:before{
-        font-size: var(--trbs-size--s);
-    }
-    trbs-timeline__step[digit-number="4"]:before{
-        font-size: var(--trbs-size--xs);
-    }
-    trbs-timeline__finish-flag{
-        top: auto;
-        bottom: 0;
-        animation: TrbsIconRotater 1s infinite;
-    }
-    .trbs-timeline--row trbs-timeline__step:before,
-    .trbs-timeline--row trbs-timeline__finish-flag{
-        top: -4.1rem;
-        left: auto;
-    }
-    
-    .trbs-timeline--row trbs-timeline__finish-flag{
-        right: 0;
-        bottom: auto;
-    }
-    @keyframes TrbsIconRotater{
-        0%{
-            transform:rotate(30deg);
-        }
-        50%{
-            transform:rotate(-30deg);
-        }
-        100%{
-            transform:rotate(30deg);
-        }
-    }
-`;
-document.head.appendChild(TrbsTimelineStyle);
-
-customElements.define('trbs-timeline', class TrbsTimeline extends HTMLElement{
+customElements.define('trds-timeline', class TrdsTimeline extends HTMLElement{
     
     constructor(){ 
         super();
 
-        this.$finishFlagWrapper = document.createElement('trbs-timeline__finish-flag');
-        this.$finishFlagWrapper.innerHTML = `
-            <trbs-icon icon="solid/flag-checkered"></trbs-icon>
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = `
+            <style>
+                :host{
+                    display: grid;
+                    border-left: 3px solid var(--trds-theme--primary);
+                    gap: var(--trds-space--xl);
+                    padding-left: 2rem;
+                    margin-left: 1rem;
+                    box-sizing: border-box;
+                    position: relative;
+                }
+                :host(.row-oriented){
+                    grid-template-columns: repeat(auto-fit, minmax(1px, 1fr));
+                    border-left: none;
+                    border-top: 3px solid var(--trds-theme--primary);
+                    padding-left: 0;
+                    margin-left: 0;
+                    padding-top: 3rem;
+                }
+                ::slotted(trds-timeline__step){
+                    display: block;
+                    position: relative;
+                }
+                ::slotted(trds-timeline__step:last-child){
+                    padding-bottom: 2.85rem;
+                }
+                :host(.row-oriented) ::slotted(trds-timeline__step:last-child){
+                    padding-bottom: 0;
+                }
+                ::slotted(trds-timeline__step):before,
+                trds-timeline__finish-flag{
+                    content: attr(number);
+                    width: 2rem;
+                    height: 2rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    background-color: var(--trds-theme--primary);
+                    border-radius: 50%;
+                    position: absolute;
+                    left: -3.05rem;
+                    top: -.28rem;
+                    font-size: var(--trds-size--m);
+                    font-weight: bold;
+                }
+                ::slotted(trds-timeline__step[digit-number="3"]):before{
+                    font-size: var(--trds-size--s);
+                }
+                ::slotted(trds-timeline__step[digit-number="4"]):before{
+                    font-size: var(--trds-size--xs);
+                }
+
+                trds-timeline__finish-flag{
+                    top: auto;
+                    left: -1.05rem;
+                    bottom: 0;
+                    animation: TrdsIconRotater 1s infinite;
+                }
+                :host(.row-oriented) ::slotted(trds-timeline__step):before{
+                    top: -4.1rem;
+                    left: auto;
+                }
+                :host(.row-oriented) trds-timeline__finish-flag{
+                    top: -1.1rem;
+                    left: auto;
+                }
+                :host(.row-oriented) trds-timeline__finish-flag{
+                    right: 0;
+                    bottom: auto;
+                }
+                @keyframes TrdsIconRotater{
+                    0%{
+                        transform:rotate(30deg);
+                    }
+                    50%{
+                        transform:rotate(-30deg);
+                    }
+                    100%{
+                        transform:rotate(30deg);
+                    }
+                }
+            </style>
+            <slot></slot>
+            <trds-timeline__finish-flag>
+                <trds-icon icon="solid/flag-checkered"></trds-icon>
+            </trds-timeline__finish-flag>
         `;
 
     }
@@ -100,18 +105,16 @@ customElements.define('trbs-timeline', class TrbsTimeline extends HTMLElement{
     connectedCallback(){
 
         let resizeTimer;
-        let timelineSteps = this.querySelectorAll('trbs-timeline__step');
+        let timelineSteps = this.querySelectorAll('trds-timeline__step');
 
         timelineSteps.forEach(elem => {
-            if(!elem.hasAttribute('number')) return console.error("Number attribute must be added to trbs-timeline__step component.");
+            if(!elem.hasAttribute('number')) return console.error("Number attribute must be added to trds-timeline__step component.");
             if(elem.getAttribute('number').length > 4) return console.error("Number attribute should not exceed 4 digits.");
             elem.setAttribute( 'digit-number', elem.getAttribute('number').length);
         });
 
-        this.querySelector('trbs-timeline__step:last-child').appendChild(this.$finishFlagWrapper);
-
         const setTimeline = () => {
-            (this.parentElement.getBoundingClientRect().width / 280) >= timelineSteps.length ? this.classList.add('trbs-timeline--row') : this.classList.remove('trbs-timeline--row');
+            (this.parentElement.getBoundingClientRect().width / 280) >= timelineSteps.length ? this.classList.add('row-oriented') : this.classList.remove('row-oriented');
         };
 
         setTimeline();
