7443ac581c3eb0b375a0bd68182bf186cde5ab5a
Daniel Kormos <danielkormos1993@gmail.com>
2022-01-15 22:12:02 +0100

    improve page score

--- build.js
+++ build.js
@@ -5,7 +5,21 @@ const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const uglify = require('gulp-uglify-es').default;
 
-gulp.src('./src/**/*.css')
+gulp.src([
+    './src/theme.css',
+    './src/layout/body.css'
+    ])
+    .pipe(concat('trds-critical.css'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(cleanCSS())
+    .pipe(rename('trds-critical.min.css'))
+    .pipe(gulp.dest('./dist/'));
+
+gulp.src([
+    './src/**/*.css',
+    '!./src/theme.css',
+    '!./src/layout/body.css'
+    ])
     .pipe(concat('trds.css'))
     .pipe(gulp.dest('./dist/'))
     .pipe(cleanCSS())
