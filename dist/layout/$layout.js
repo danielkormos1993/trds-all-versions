02bf50f68ab1daae64d8e0f07e66317e4e207c92
danielkormos1993 <danielkormos1993@gmail.com>
2022-10-29 10:14:26 +0200

    0.1.2

--- dist/layout/$layout.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
-var _createStyle = _interopRequireDefault(require("../libs/createStyle"));
-(0, _createStyle.default)("\n\n:root{\n\n    --space--xs: 5px;\n    --space--s: 10px;\n    --space--m: 20px;\n    --space--l: 30px;\n    --space--xl: 40px;\n    --space--xxl: 90px;\n\n    --element--max-width: 800px;\n\n}\n\n");
--- dist/typhography/$typhography.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault").default;
-var _createStyle = _interopRequireDefault(require("../libs/createStyle"));
-(0, _createStyle.default)("\n\n:root{\n\n    font-size: clamp(16px, 15.065px + 0.259vw, 20px);\n    line-height: 1.5;\n    -webkit-text-size-adjust: none;\n\n    --size--xs: .75rem;\n    --size--xs--line-height: 1.55;\n\n    --size--s: .9rem;\n    --size--s--line-height: 1.52;\n\n    --size--m: 1rem;\n    --size--m--line-height: 1.5;\n\n    --size--l: 1.2rem;\n    --size--l--line-height: 1.4;\n\n    --size--xl: 2rem;\n    --size--xl--line-height: 1.3;\n\n    --size--xxl: clamp(2rem, 1.7662rem + 1.0390vw, 3rem);\n    --size--xxl--line-height: 1.2;\n\n}\n\n");
